77f358bb367ff4da2401b412bf20627ad06b1ade
Cameron Williams <cameronrw.nz@gmail.com>
2024-05-11 08:10:59 +0700

    Hiding practice translation on next and fixing height of practice card

--- app/practice/cards.tsx
+++ app/practice/cards.tsx
@@ -9,17 +9,18 @@ import { Translation, TranslationLanguage } from "../common/translation";
 const translations = ProcessTranslations();
 
 export function Cards() {
-    const [isShowingSentence, setIsShowingSentence] = useState<boolean>();
+    const [isShowingSentence, setIsShowingSentence] = useState<boolean>(true);
     const [isShowingTranslation, setIsShowingTranslation] = useState<boolean>();
     const [translation, setTranslation] = useState<ITranslation>(translations[Math.floor(Math.random() * translations.length)]);
 
     function nextTranslation() {
         setTranslation(translations[Math.floor(Math.random() * translations.length)])
+        setIsShowingTranslation(false)
     }
 
     return (
         <Container className="space-y-4 m-1">
-            <Container className="space-y-1">
+            <Container className="space-y-1 h-36">
                 <Translation language={TranslationLanguage.Thai} size="large">{translation.thai}</Translation>
                 {isShowingSentence ? <Translation language={TranslationLanguage.Thai} size="small">{translation.sentence}</Translation> : null}
                 {isShowingTranslation ? <Translation language={TranslationLanguage.English} size="large">{translation.english.join(" / ")}</Translation> : null}
